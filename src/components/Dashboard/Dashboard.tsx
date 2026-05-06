3def01e40e1f61cf571316ded77be017e9c86513
fdiallo <fallaye.diallo@gmail.com>
2026-05-06 13:14:41 -0400

    cleaned up code

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -37,10 +37,7 @@ function Dashboard() {
             <h2>Welcome to the Task Manager App! </h2>
             <br />
 
-
-
            <TaskForm onTaskAdded = {handleTaskAdd}  />    
-
 
             {/**
       * TaskFilter component allows users to filter tasks based on their status and priority.
@@ -104,9 +101,6 @@ function Dashboard() {
                      setTaskList(taskList.filter(task => task.id !== taskId))
                 }
                 }
-
-                 
-
             />
 
         </div>
@@ -114,64 +108,3 @@ function Dashboard() {
 }
 
 export default Dashboard
-
-
-
-
-
-
-// interface User{
-//     id: number,
-//     name: string,
-//     email: string
-// }
-
-// interface UserProps{
-//     user: User
-// }
-
-// const users: User[] =[
-//     {
-//     id: 0,
-//     name: "Paul",
-//     email: "PaulIsReallyRad@perscholas.org"
-// },
-//     {
-//     id: 1,
-//     name: "Quinn",
-//     email: "QuinnTriesHard@perscholas.org"
-// },
-//     {
-//     id: 2,
-//     name: "Ta'Ron",
-//     email: "SeniorDev@taron.biz"
-// },
-//     {
-//     id: 3,
-//     name: "Jasmine",
-//     email: "SeniorDev@jasmine.biz"
-// },
-// ]
-
-// function UserProfileCard({user}: UserProps ){
-//     return(
-//         <div>
-//         <h3>{user.name}</h3>
-//         <p>{user.email}</p>
-//         </div>
-//     )
-// }
-
-// function UserList(){
-//     const usersElements = users.map((user) => <UserProfileCard key={user.id} user={user}/>
-
-//     )
-//  return(
-//     <>
-//     <h1>UserList</h1>
-//     {usersElements}
-//     </>
-//  )
-// }
-
-// export default UserList
